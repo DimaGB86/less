e536cc6f75f9ee2437bae4639f137ac8f8a25f0a
Dima <sh_diman@inbox.ru>
2023-03-25 16:10:25 +0500

    Задачка1

---
+++ Less_10/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 22, 33, 43, 54, 53, 64, 54, 32};
+
+int n = array.Length;
+int find = 54;
+
+int index = 0;
+
+while (index < n)
+{
+
+  if(array[index] == find)
+  {
+     Console.WriteLine(index);
+     break;
+  }
+   //index = index + 1;
+   index++;
+}
