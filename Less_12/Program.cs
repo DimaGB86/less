54e4368ddc27f0eedf663c176b3ec93a51537492
Dima <sh_diman@inbox.ru>
2023-04-02 13:10:48 +0500

    Методы и функции

---
+++ Less_12/Program.cs
@@ -0,0 +1,53 @@
+﻿//Методы функции
+ // Вид 1.
+
+// void Methood1()
+// {
+//   Console.WriteLine("Автор ...");
+// }
+// Methood1();
+
+
+// Вид 2
+
+// void Methood2(string msg)
+// {
+//   Console.WriteLine(msg);
+// }
+// Methood2(msg: "Текст сообщения");
+// пример 2
+// void Methood21(string msg, int count)
+// {
+//   int i = 0;
+//   while (i<count)
+//   {
+//     Console.WriteLine(msg);
+//     i++;
+//   }
+// }
+// Methood21("Текст", 4);
+
+// Вид 3
+
+// int Methood3()
+// {
+//   return DateTime.Now.Year;
+// }
+// int year = Methood3();
+// Console.WriteLine(year);
+
+// Вид 4
+
+ string Methood4(int count, string text)
+ {
+  int i = 0;
+  string result = string.Empty;
+  while(i < count)
+  {
+    result = result + text;
+    i++;
+  }
+  return result;
+ }
+ string res = Methood4(10, "Dima");
+ Console.WriteLine(res);
